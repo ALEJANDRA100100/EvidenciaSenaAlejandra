34e4eaa7a2d95819bf7a8cf918627eaee770af15
ALEJANDRA100100 <ramirezalejandra368@gmail.com>
2025-09-03 19:46:18 -0500

    Agrego saludo en ingles y frances

--- holaevidencia.py
+++ holaevidencia.py
@@ -2,3 +2,14 @@ def saludar(nombre):
     print ("Hola "+ nombre)
 
 saludar("Alejandra")
+
+def saludarIngles(nombre):
+    print ("Hi "+ nombre)
+saludarIngles("Alejandra")
+
+def saludarFrances(nombre):
+    print ("Bonjour "+ nombre)      
+
+saludarFrances("Lucrecia")
+
+
