224f13f667c6f13a6c18e89d277be7d95be0d520
ALEJANDRA100100 <ramirezalejandra368@gmail.com>
2025-09-09 19:43:01 -0500

    Operaciones suma, resta, multiplicacion y division

--- holaevidencia.py
+++ holaevidencia.py
@@ -22,4 +22,24 @@ resultado = ElevarAlCubo(3)
 print (resultado) 
 
 
+def sumar(num1, num2):
+    return num1 + num2
+resultado = sumar(5, 7)
+print (resultado)
+
+def restar(num1, num2):
+    return num1 - num2      
+resultado = restar(10, 4)
+print (resultado)
+
+def multiplicar(num1, num2):
+    return num1 * num2      
+resultado = multiplicar(6, 8)
+print (resultado)
+
+def dividir(num1, num2):
+    return num1 / num2      
+resultado = dividir(20, 5)
+print (resultado)   
+
 
